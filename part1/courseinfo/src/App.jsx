ea08c458de4e6b90f5798ea8946fb3c9222e4ce1
Neider Galofre Morales <desarrollador2@unicurn.edu.co>
2024-07-19 10:45:51 -0500

    Part1

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,53 @@
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+const Header = ({ course }) => {
+  return (<h1>{course}</h1>)
+}
+
+const Content = ({ parts }) => {
+  return <div>
+    {
+      parts.map(part => <Part key={part.name} part={part} />)
+    }
+  </div>
+}
+
+
+const Part = ({ part }) => {
+  return (
+    <p>{part.name} {part.exercises}</p>
+  )
+}
+
+const Total = ({ parts }) => {
+  return (
+    <p>Number of exercises {parts.reduce((total, part) => total + part.exercises, 0)}</p>
+  )
+}
+
+export default App
---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+
+const App = () => {
+  // guarda los clics de cada botón en su propio estado
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={() => setGood(good + 1)} text={'good'} />
+      <Button onClick={() => setNeutral(neutral + 1)} text={'neutral'} />
+      <Button onClick={() => setBad(bad + 1)} text={'bad'} />
+
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+const Statistics = ({ good, neutral, bad }) => {
+  if (good === 0 && neutral === 0 && bad === 0) {
+    return <p>No feedback given</p>
+  }
+
+  const all = good + neutral + bad
+
+  return <table>
+    <tbody>
+      <StatisticLine text='good' value={good} />
+      <StatisticLine text='neutral' value={neutral} />
+      <StatisticLine text='bad' value={bad} />
+      <StatisticLine text='all' value={all} />
+      <StatisticLine text='average' value={(good - bad) / all} />
+      <StatisticLine text='positive' value={(good / all) * 100} />
+    </tbody>
+  </table>
+}
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
+export default App
