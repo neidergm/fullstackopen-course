11944c8a8e9bb9c537e04bcfec1a788e490d05a1
Neider Galofre Morales <desarrollador2@unicurn.edu.co>
2024-08-21 19:28:09 -0500

    part5 - 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,34 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const blogStyle = {
+  padding: 10,
+  borderRadius: "5px",
+  marginBottom: 5,
+  background: "lightgray"
+}
+
+const Blog = ({ blog }) => {
+
+  const [showDetails, setShowDetails] = useState(false);
+
+  const toggleDetails = () => setShowDetails(s => !s)
+
+  return (
+    <div style={blogStyle}>
+      {blog.title}
+      <button onClick={toggleDetails}>
+        {showDetails ? 'hide' : 'view'}
+      </button>
+
+      <div style={showDetails ? {} : { display: 'none' }}>
+        <p> {blog.url}</p>
+        <p> {blog.likes} likes
+          <button>like</button>
+        </p>
+        <p> {blog.author}</p>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
